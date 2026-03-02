24466d30ca2277d4e4bb1a2f8eb8c49f3cd40775
mostlykiguess <bruvistrue93@gmail.com>
2026-03-02 18:15:41 +0530

    Zed's flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,16 @@
 {
-  description = "Cached zed-editor builds";
+  description = "Cached Zed editor builds";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default =
-      nixpkgs.legacyPackages.x86_64-linux.zed-editor;
+  inputs = {
+    zed.url = "github:zed-industries/zed";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, zed, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages = {
+        default = zed.packages.${system}.default;
+        zed-editor = zed.packages.${system}.default;
+      };
+    });
 }
